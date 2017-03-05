4d2d27172fba1fd54561aef95a45139075039f62
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-03-05 20:26:49 +0100

    natrium: Add fingerprint
    Change-Id: If3b2527585ec32caddc457e1293f86fd27b6f1af

--- lineage.mk
+++ lineage.mk
@@ -3,9 +3,16 @@ $(call inherit-product, device/xiaomi/natrium/full_natrium.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_natrium
 PRODUCT_BRAND := xiaomi
-PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_DEVICE := natrium
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_MODEL := Mi 5s Plus
+PRODUCT_NAME := lineage_natrium
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="natrium" \
+    PRODUCT_NAME="natrium" \
+    BUILD_FINGERPRINT="Xiaomi/natrium/natrium:6.0.1/MXB48T/V8.0.4.0.MBGMIDI:user/release-keys" \
+    PRIVATE_BUILD_DESC="natrium-user 6.0.1 MXB48T V8.0.4.0.MBGMIDI release-key"
