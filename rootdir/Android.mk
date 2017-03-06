a910e9a373e96c42b8419ad996c6f3ab1720ad07
LuK1337 <priv.luk@gmail.com>
2017-03-06 12:54:45 +0100

    natrium: Add device specific init rc
    Change-Id: I7456b7e3eaf340a5e193ca2bb485dc141536e30a

---
+++ rootdir/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# Device init scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.target.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.target.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
