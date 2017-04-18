c52daf8903554561913d8fe5da4d94a7ae9cc2e9
Luca Stefani <luca020400@lineageos.org>
2017-04-18 13:23:35 +0200

    natrium: Build OSS consumerir hal
    Change-Id: I9601a58034b7b504232cb830c85c52356364d78f

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml
 
+# Consumerir
+PRODUCT_PACKAGES += \
+    consumerir.msm8996
+
 # Device init scripts
 PRODUCT_PACKAGES += \
     init.target.rc
