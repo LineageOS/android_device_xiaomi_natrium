99e219b6dc780fbc407a356ede4b67da92f12df3
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    natrium: Enforce vendor tree presence
    A compiled build has no chance of booting without the prebuilt
    binaries, so fail the build if those are not present.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 # call the proprietary setup
-$(call inherit-product-if-exists, vendor/xiaomi/natrium/natrium-vendor.mk)
+$(call inherit-product, vendor/xiaomi/natrium/natrium-vendor.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
