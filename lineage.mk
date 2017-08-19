b7da04ea0e791c2a2879e5a5a039e7349c8d822b
Luca Stefani <luca020400@lineageos.org>
2017-08-19 13:17:46 +0200

    natrium: Inherit from Lineage vendor
    Change-Id: I6d60ee5643181781ba8b386f1f16cfb59610f35c

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/xiaomi/natrium/full_natrium.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_natrium
 PRODUCT_DEVICE := natrium
