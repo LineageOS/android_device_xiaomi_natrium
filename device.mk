321e05fa5d41392eaf13322b36364b75d49f8a46
Bruno Martins <bgcngm@gmail.com>
2017-12-23 21:42:51 +0000

    natrium: Enable binderized IR HAL service
    Change-Id: I850f2bc6f139fb27ccc4d907632743f3ab9cc696

--- device.mk
+++ device.mk
@@ -34,6 +34,7 @@ PRODUCT_COPY_FILES += \
 # Consumerir
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service \
     consumerir.msm8996
 
 # Device init scripts
