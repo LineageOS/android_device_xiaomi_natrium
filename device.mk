1cc42360985e59e6d7cb126a9cc36916ef90b369
wolfwork <wolfdate25@gmail.com>
2018-02-24 15:32:19 +0000

    omnirom

--- device.mk
+++ device.mk
@@ -20,7 +20,6 @@ $(call inherit-product, vendor/xiaomi/natrium/natrium-vendor.mk)
 # Local overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
