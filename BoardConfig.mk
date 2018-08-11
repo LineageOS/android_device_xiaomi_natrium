63de84cfddd2dbf1af8892437e756e20dfe1d4f8
Bruno Martins <bgcngm@gmail.com>
2018-08-11 01:07:49 +0100

    natrium: Set vendor props via PRODUCT_PROPERTY_OVERRIDES
    * Support for vendor.prop was abandoned
    
    Change-Id: I3554bfc57c27555f8c605e8555ea5f386bce0c5d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,8 +30,5 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 # Kernel
 TARGET_KERNEL_CONFIG := natrium_defconfig
 
-# Properties
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-
 # inherit from the proprietary version
 -include vendor/xiaomi/natrium/BoardConfigVendor.mk
