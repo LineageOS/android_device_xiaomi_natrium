4a85aef94096d4c0bbb9c1ca7970a15df7ddf5f5
Bruno Martins <bgcngm@gmail.com>
2022-07-04 23:44:14 +0100

    natrium: Include target config fragment
    Change-Id: I994b7cefa73c3b06082abb69aa275a41e8845149

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2018,2020-2021 The LineageOS Project
+# Copyright (C) 2017-2018,2020-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
-TARGET_KERNEL_CONFIG := natrium_defconfig
+TARGET_KERNEL_CONFIG += vendor/xiaomi/natrium.config
 
 # inherit from the proprietary version
 -include vendor/xiaomi/natrium/BoardConfigVendor.mk
