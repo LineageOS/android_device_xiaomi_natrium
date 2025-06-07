5862d839c379a34b32ecbbebd6a04f1d61f728fe
Bruno Martins <bgcngm@gmail.com>
2025-06-07 09:26:51 +0100

    natrium: Switch to common Touch AIDL from hardware/xiaomi
    Change-Id: Ic9512ce8391887dba23df769d3c3896d1a7051a3

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2020,2025 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -62,7 +62,9 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Touch HAL
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi_8996
+    vendor.lineage.touch-service.xiaomi
+
+$(call soong_config_set,XIAOMI_TOUCH,KEY_DISABLER_CONTROL_PATH,/proc/touchpanel/capacitive_keys_enable)
 
 # Thermal
 PRODUCT_COPY_FILES += \
